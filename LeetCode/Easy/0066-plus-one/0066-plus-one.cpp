be38d290cea1805d57d35950cb7475e6bb89d25f
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-09 16:33:45 +0530

    Time: 0 ms (100%), Space: 11.6 MB (27.53%) - LeetHub

--- LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
@@ -4,7 +4,7 @@ public:
         int n = digits.size();
         int carry{1};
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0 && carry == 1; i--) {
             if (carry == 1) {
                 if (digits[i] + 1 == 10) {
                     digits[i] = 0;
@@ -15,9 +15,11 @@ public:
                 }
             }
         }
+        
         if (carry == 1) {
             digits.insert(digits.begin(), 1);
         }
+        
         return digits;
     }
 };
