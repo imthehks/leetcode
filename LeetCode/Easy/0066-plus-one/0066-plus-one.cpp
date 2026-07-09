2a52592efbc83eed05d7c0541a07425743de8cfe
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-09 16:25:40 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.94%) - LeetHub

---
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int carry{1};
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (carry == 1) {
+                if (digits[i] + 1 == 10) {
+                    digits[i] = 0;
+                    carry = 1;
+                } else {
+                    digits[i]++;
+                    carry = 0;
+                }
+            }
+        }
+        if (carry == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
