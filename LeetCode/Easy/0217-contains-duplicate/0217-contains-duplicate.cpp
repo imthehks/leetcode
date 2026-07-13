443d2ba3568145eac01217a1642cda8316fb90b8
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-13 18:49:47 +0530

    Time: 35 ms (78.73%), Space: 82.8 MB (80.98%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
+        
         sort(nums.begin(),nums.end());
         int left{0};
         int right{1};
         for(int i=0;i<n && left<n-1 && right<n;i++){
             if(nums[left]==nums[right]){
                 return true;
+                break;
             }else{
                 left++;
                 right++;
@@ -15,5 +17,10 @@ public:
             
         }
         return false;
+        
+    
+        
+
+
     }
 };
