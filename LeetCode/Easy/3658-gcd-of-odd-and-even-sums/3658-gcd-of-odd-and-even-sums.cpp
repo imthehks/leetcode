cfc33f4d5c12516896158fa923a5e9352eb15961
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-15 10:09:11 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.02%) - LeetHub

--- LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        // return n;  // -> first approach
+
+        // 2nd approach
+        int sumEven = n * n;
+        int sumOdd = n * n + n;
+        return gcd(sumEven, sumOdd);
     }
 };
