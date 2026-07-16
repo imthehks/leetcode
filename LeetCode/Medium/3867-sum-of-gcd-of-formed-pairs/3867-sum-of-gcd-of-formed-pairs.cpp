6f8f9b279fe938ef5ca03dd9f96851ea94d2521f
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-16 09:19:44 +0530

    Time: 78 ms (43.32%), Space: 155.4 MB (95.24%) - LeetHub

---
+++ LeetCode/Medium/3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> prefixGcd(n);
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+            prefixGcd[i] = gcd(nums[i], maxi);
+        }
+
+        //  sorting prefixGcd in ascending order
+        sort(prefixGcd.begin(), prefixGcd.end());
+
+        //  have to make pairs of smallest one with the largest one , until no
+        //  element is left
+
+        int left = 0;
+        int right = prefixGcd.size() - 1;
+        long long totalSum = 0;
+
+        while (left < right) {
+            totalSum += gcd(prefixGcd[left], prefixGcd[right]);
+            left++;
+            if (n % 2 != 0) {
+                int mid = (n + 1) / 2;
+                if (left == mid) {
+                    break;
+                }
+            }
+
+            right--;
+        }
+        return totalSum;
+    }
+};
