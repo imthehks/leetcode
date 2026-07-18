633fbe680c61fb0a411f5e68eef5c5bf89242212
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-18 09:31:49 +0530

    Time: 2 ms (10.69%), Space: 27.7 MB (44.63%) - LeetHub

--- LeetCode/Easy/3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ LeetCode/Easy/3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minElement(vector<int>& nums) {
-        int rem {0};
         for(int i=0; i<nums.size(); i++){
             int num = nums[i];
-            rem = 0;
+            int rem {0};
             while(num>0){
                 rem += num%10;
                 num=num/10;  
