d312b716fd0641532901a23730ff152f336325f2
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-21 08:57:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (60.6%) - LeetHub

--- LeetCode/Easy/0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ LeetCode/Easy/0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> result;
-        bool mark = true;
+        
         for (int i = left; i <= right; i++) {
-           mark = true;
+          bool mark = true;
             int num = i;
             while (num > 0) {
                 int rem = num % 10;
