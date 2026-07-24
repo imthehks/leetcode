645993885cee3b806e1d04186b9835c2190b8c58
Himanshu Kumar <himanshukumar4736@gmail.com>
2026-07-24 10:54:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (7.76%) - LeetHub

--- LeetCode/Easy/1518-water-bottles/1518-water-bottles.cpp
+++ LeetCode/Easy/1518-water-bottles/1518-water-bottles.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int totalDrinks = numBottles;
-        int emptyDrinks = numBottles;
-        while (emptyDrinks >= numExchange) {
-            int quo = emptyDrinks / numExchange;
-            int rem = emptyDrinks % numExchange;
-            totalDrinks += quo;
-            emptyDrinks = quo + rem;
+        while (numBottles >= numExchange) {
+            totalDrinks +=numBottles / numExchange;
+            numBottles = ((numBottles / numExchange)+ (numBottles % numExchange));
         }
         return totalDrinks;
     }
